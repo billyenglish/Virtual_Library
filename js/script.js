d30d0bef975cac15cb4ddba79a10d39a95c08ece
Billy English <billyenglish6212@outlook.com>
2024-09-04 12:45:02 -0400

    Adding functionality to the virtual card for the submit button

--- js/script.js
+++ js/script.js
@@ -1,34 +1,104 @@
-const lightModeButton = document.querySelector('.fa-sun');
+const signIn = document.querySelector(".sign-in");
+const userSignIn = document.querySelector(".dialog-signin");
+const closeSignIn = document.querySelector(".close-signin");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const addBook = document.querySelector("#add-book");
+const dialogBookForm = document.querySelector(".dialog-bookform");
+const closeBookForm = document.querySelector(".close-form");
+const cancelSignIn = document.querySelector("#cancel-signin");
+const cancelBookForm = document.querySelector("#cancel-bookform");
+const bookTitle = document.querySelector("#title")
+const bookAuthor = document.querySelector("#author");
+const numberOfPages = document.querySelector("#pages");
+const readBook = document.querySelector("#read");
+const submitBook = document.querySelector("#submit-bookform");
+const bookshelf = document.querySelector("#bookshelf");
 
-lightModeButton.addEventListener('click', () => {
-    lightModeButton.style.display = "none";
-    darkModeButton.style.display = "flex";
-    alert('Light Mode');
-});
+/* User Sign In Button Opens User Sign In Form */
 
-const darkModeButton = document.querySelector('.fa-moon');
+signIn.addEventListener("click", signInForm);
 
-darkModeButton.addEventListener('click', () => {
-    darkModeButton.style.display = "none";
-    lightModeButton.style.display = "flex";
-    alert('Dark Mode')
-})
-
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-
-const openMenu = () => {
-    const formMenu = document.querySelector('.menu');
-    formMenu.style.display = "flex";
+function signInForm() {
+    userSignIn.showModal();
 }
 
-hamburgerMenu.addEventListener('click', openMenu);
+/* Close User Sign In Form */
 
-const closeIcon = document.querySelector('.close-icon');
+closeSignIn.addEventListener("click", closeUserSignInForm); 
 
-const closeMenu = () => {
-    const formMenu = document.querySelector('.menu');
-    formMenu.style.display = "none";
+function closeUserSignInForm() {
+    userSignIn.close();
+    clearUserSignInForm();
+}
+
+/* Cancel User Sign In */
+
+function clearUserSignInForm() {
+
+    username.value = "";
+    password.value = "";
+
     return;
 }
 
-closeIcon.addEventListener('click', closeMenu);
+cancelSignIn.addEventListener("click", clearUserSignInForm);
+
+/* Open Virtual Book Form */
+
+function openBookForm() {
+    dialogBookForm.showModal();
+}
+
+addBook.addEventListener("click", openBookForm);
+
+/* Close Virtual Book Form */
+
+function closeBook() {
+    dialogBookForm.close();
+}
+
+closeBookForm.addEventListener("click", closeBook);
+
+/* Clear Virtual Book Form */
+
+function clearBookForm() {
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    numberOfPages.value = "";
+    readBook.checked = "";
+
+    return;
+}
+
+/* Cancel Book Form */
+
+cancelBookForm.addEventListener("click", clearBookForm);
+
+/* Create Virtual Book */
+
+const myLibrary = [];
+
+function Book(title, author, book, read) {
+    this.title = title;
+    this.author = author;
+    this.book = book;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let createTitle = bookTitle.value;
+    let createAuthor = bookAuthor.value;
+    let createBookPages = numberOfPages.value;
+    let createReadBook = readBook.value;
+
+    const createBook = new Book(createTitle, createAuthor, createBookPages, createReadBook);
+
+    myLibrary.push(createBook);
+
+    console.log(myLibrary);
+}
+
+console.log(addBookToLibrary())
+
+submitBook.addEventListener("click", addBookToLibrary);
