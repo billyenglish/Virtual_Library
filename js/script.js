a98cf30442cd6a8b802b9a9e8627fbeab5f099f1
Billy English <billyenglish6212@outlook.com>
2024-09-05 12:39:48 -0400

    Add clear form function for sign and book form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const userInputs = document.querySelectorAll("input");
 
-const clearUserInput = (inputs) => {
-    inputs.forEach((input) => {
-        inputs.
-    });
+const clearForm = (inputs) => {
+    inputs.forEach(input => input.type === "checked" ? checked = false : input.value = "");
 }
 
 /* User Sign In Form Open and Closing Form */
@@ -11,7 +9,9 @@ const clearUserInput = (inputs) => {
 const signInButton = document.querySelector("#sign-in");
 const signInModal = document.querySelector(".dialog-signin");
 const closeSignIn = document.querySelector(".close-signin");
-const userInput = document.querySelector('input');
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const cancelSignIn = document.querySelector("#cancel-signin");
 
 const openUserSignIn = () => {
     signInModal.showModal();
@@ -21,15 +21,25 @@ signInButton.addEventListener("click", openUserSignIn);
 
 const closeUserSignIn = () => {
     signInModal.close();
+    clearForm([username, password]);
 }
 
 closeSignIn.addEventListener("click", closeUserSignIn);
 
-/* Add Book Form for Open and close function */
+cancelSignIn.addEventListener("click", () => {
+    signInModal.close();
+    clearForm([username, password]);
+});
 
 const addBookButton = document.querySelector("#add-book");
 const addBookForm = document.querySelector(".dialog-bookform");
 const closeBookForm = document.querySelector(".close-form");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author")
+const bookPages = document.querySelector("pages");
+const isRead = document.querySelector("#read");
+
+/* Add Book Form for Open and close function */
 
 addBookButton.addEventListener("click", () => {
     addBookForm.showModal();
@@ -37,5 +47,6 @@ addBookButton.addEventListener("click", () => {
 
 closeBookForm.addEventListener("click", () => {
     addBookForm.close();
+    clearForm([bookTitle, bookAuthor, bookPages, read]);
 })
 
