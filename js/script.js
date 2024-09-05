adc38fd823ed174b2843b5f89335b43265e80899
Demar English <demarenglish@Mac.attlocal.net>
2024-09-04 21:15:37 -0400

    Mapped out the functionality with the creation of the Library

--- js/script.js
+++ js/script.js
@@ -1,91 +1,45 @@
-/* Created Opening Sign In */
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-const signIn = document.querySelector(".sign-in");
-const signinForm = document.querySelector(".dialog-signin");
+    bookTitle() {
+        return this.title === "" ? "Please Enter A Book Title" : this.title;
+    }
 
-signIn.addEventListener("click", openSignIn);
+    bookAuthor() {
+        return this.author === "" ? "Please Enter A Book Author" : this.author;
+    }
 
-function openSignIn() {
-    signinForm.showModal();
+    bookPages() {
+        return this.pages > 0 ? "Please Valid Number of Book Pages" : this.pages;
+    }
+
+    isRead() {
+        return this.read;
+    }
 }
-
-/* Created Closing Sign In */
-
-const closeSignIn = document.querySelector(".close-signin");
-
-closeSignIn.addEventListener("click", closeSignInForm);
-
-function closeSignInForm() {
-    signinForm.close();
-}
-
-/* Clear User Sign In Form */
-
-const clearSignIn = document.querySelector("#cancel-signin");
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
-
-clearSignIn.addEventListener("click", clearSignInForm);
-
-function clearSignInForm() {
-    usernameInput.value = "";
-    passwordInput.value = "";
-    signinForm.close();
-
-    return;
-}
-
-/* Opening Book Form */
-
-const addBook = document.querySelector("#add-book");
-const openBookForm = document.querySelector(".dialog-bookform");
-
-addBook.addEventListener("click", toggleBookForm);
-
-function toggleBookForm() {
-    openBookForm.showModal();
-}
-
-/* Close Book Form */
-
-const closeForm = document.querySelector(".close-form");
-const bookForm = document.querySelector(".dialog-bookform");
-
-closeForm.addEventListener("click", closeBookForm);
-
-function closeBookForm() {
-    bookForm.close();
-}
-
-/* Clear Book Form */
-
-const bookTitle = document.querySelector("#title");
-const bookAuthor = document.querySelector("#author");
-const bookPages = document.querySelector("#pages");
-const bookIsRead = document.querySelector("#read");
-const cancelBookForm = document.querySelector("#cancel-bookform");
-
-cancelBookForm.addEventListener("click", cancelBookFormToggle);
-
-function cancelBookFormToggle() {
-    bookTitle.value = "";
-    bookAuthor.value = "";
-    bookPages.value = "";
-    bookIsRead.checked = false;
-    bookForm.close();
-}
-
-/* Creating Library */
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function addToLibrary(bookTitle, bookAuthor, bookPages, bookIsRead) {
+    let createBook = new Book(bookTitle, bookAuthor, bookPages, bookIsRead);
+    myLibrary.push(createBook);
+    return myLibrary;
 }
 
-function addToLibrary() {
+function createBook() {
+    let result = addToLibrary('Title', 'Author', 'Pages', 'isRead');
 
-}
+    result.forEach(book => {
+        console.log(book.bookTitle());
+        console.log(book.bookAuthor());
+        console.log(book.bookPages());
+        console.log(book.isRead());
+    })
+;}
+
+console.log(createBook());
