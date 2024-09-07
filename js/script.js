1bfa56da566459f5513632a8491153c557cf3fa2
Demar English <demarenglish@Mac.attlocal.net>
2024-09-07 04:19:19 -0400

    Added functionality for clear book form

--- js/script.js
+++ js/script.js
@@ -8,9 +8,35 @@ const showBookForm = () => {
 createBook.addEventListener("click", showBookForm);
 
 const closeIcon = document.querySelector(".close-bookform");
+const inputs = document.querySelectorAll("input");
 
 const closeBookForm = () => {
     dialogBook.close();
+    clearForm(inputs);
 }
 
 closeIcon.addEventListener("click", closeBookForm);
+
+const cancelButton = document.querySelector("#cancel");
+
+const clearForm = (inputs) => {
+    inputs.forEach(input => {
+        if (input.id === "title") {
+            input.value = "";
+        }
+
+        if (input.id === "author") {
+            input.value = "";
+        }
+
+        if (input.id === "pages") {
+            input.value = "";
+        }
+
+        if (input.id === "read") {
+            input.checked = "";
+        }
+    });
+}
+
+cancelButton.addEventListener("click", () => clearForm(inputs));
