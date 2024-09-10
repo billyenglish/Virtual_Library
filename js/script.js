8cb4cb8fb8a8340c94c82514af0899325567247e
Demar English <demarenglish@Mac.attlocal.net>
2024-09-10 03:43:43 -0400

    Update the functional and Error message display

--- js/script.js
+++ js/script.js
@@ -1,69 +1,30 @@
-/* Initialized Create Add Book Button and Book Form Variable */
-
-const createBook = document.querySelector("#create-book");
-const dialogBook = document.querySelector("#dialog");
-
-/* Show Book Form Opens Book Forms */
-
-const showBookForm = () => {
-    dialogBook.showModal();
-}
-
-createBook.addEventListener("click", showBookForm);
-
-/* Initialized the Close Icon and inputs */
-
-const closeIcon = document.querySelector(".close-bookform");
-const inputs = document.querySelectorAll("input");
-
-/* Close Book Form Function */
-
-const closeBookForm = () => {
-    dialogBook.close();
-    clearForm(inputs);
-}
-
-/* Close Icon Clear Inputs and Closes Forms */
-
-closeIcon.addEventListener("click", closeBookForm);
-
-/* Initialized the Cancel Button Variable */
-
-const cancelButton = document.querySelector("#cancel");
-
-/* Clear Form Functions */
-
-const clearForm = (inputs) => {
-    inputs.forEach(input => {
-        if (input.id === "title") {
-            input.value = "";
-        }
-
-        if (input.id === "author") {
-            input.value = "";
-        }
-
-        if (input.id === "pages") {
-            input.value = "";
-        }
-
-        if (input.id === "read") {
-            input.checked = "";
-        }
-    });
-}
-
-/* Cancel Button that closes the Book Form and Clears */
-
-cancelButton.addEventListener("click", () => clearForm(inputs));
-
-/* Creating Book Class */
-
+const dialog = document.querySelector("#dialog");
+const openForm = document.querySelector("#form");
+const addBook = document.querySelector("#add-book");
 const bookTitle = document.querySelector("#title");
 const bookAuthor = document.querySelector("#author");
 const bookPages = document.querySelector("#pages");
-const bookisRead = document.querySelector("#read");
+const bookRead = document.querySelector("#read");
 const bookShelf = document.querySelector("#bookshelf");
+const closeIcon = document.querySelector(".close-form")
+const cancelButton = document.querySelector("#cancel");
+
+const openBookForm = () => {
+    dialog.showModal();
+}
+
+addBook.addEventListener("click", openBookForm);
+
+const clearBookForm = () => {
+    dialog.close();
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    bookRead.checked = false;
+}
+
+closeIcon.addEventListener("click", clearBookForm);
+cancelButton.addEventListener("click", clearBookForm);
 
 class Book {
     constructor(title, author, pages, read) {
@@ -72,21 +33,44 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
+
+    isTitle() {
+    }
+
+    isAuthor() {
+    }
+
+    isPages() {
+    }
+
+    isRead() {
+    }
 }
 
 const myLibrary = [];
 
-const addBookToLibrary = () => {
-    let book = new Book('The Hobbit', 'J.K Rawlings', 454, true);
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
     myLibrary.push(book);
     return myLibrary;
 }
 
-const newBook = function () {
-    
+function invalidEntry() {
 }
 
+function createBook() {
+    let newBook = addBookToLibrary(title, author, pages, read);
 
+    newBook.forEach(book => {
+        console.log(book);
+    })
+}
 
+const submitButton = document.querySelector('#submit');
 
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBookToLibrary(`${bookTitle.value}`, `${bookAuthor.value}`, `${bookPages.value}`, `${bookRead.checked}`);
+    createBook();
+});
 
