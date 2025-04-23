5384bb4b1cc0e220310879b0cb4dd1005698111f
Billy English <billyenglish6212@outlook.com>
2025-04-22 21:03:10 -0400

    Added Factory Constructor function.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let array = [1, 2, 3, 4, 5];
+// Create Virtual Library
+// Create a Book
+// This should have the following properties: title (string), author (string), genre (string), pages (number), read (boolean)
+// This book should also have a unique ID (string) that is generated when the book is created.
+// Before creating the book check if the book already exists in the library.
+// Validate the book properties
 
-console.log(array[Math.floor(array.length / 2)]);
+
+const myLibrary = [];
+
+function Book(isTitle, isAuthor, isGenre, isPages, isRead) {
+    this.isTitle = isTitle;
+    this.isAuthor = isAuthor;
+    this.isGenre = isGenre;
+    this.isPages = isPages;
+    this.isRead = isRead;
+    this.id = crypto.randomUUID();
+}
+
