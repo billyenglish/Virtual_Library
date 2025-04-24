c830c3021899cfce8b5e6142004e6fc5c346bcb7
Demar English <demarenglish@Mac.attlocal.net>
2025-04-24 16:05:42 -0400

    Create Logic for adding books.

--- script.js
+++ script.js
@@ -8,12 +8,46 @@
 
 const myLibrary = [];
 
-function Book(isTitle, isAuthor, isGenre, isPages, isRead) {
-    this.isTitle = isTitle;
-    this.isAuthor = isAuthor;
-    this.isGenre = isGenre;
-    this.isPages = isPages;
-    this.isRead = isRead;
+function Book(title, author, genre, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.pages = pages;
+    this.read = read;
     this.id = crypto.randomUUID();
 }
 
+function addBookToLibrary(title, author, genre, pages, read) {
+
+    const newBook = new Book(title, author, genre, pages, read);
+
+    if (newBook.title !== "" && author !== "" && genre !== "" && pages > 0) {
+        myLibrary.push(newBook);
+        console.log(myLibrary);
+    } else {
+        console.log("Invalid");
+        myLibrary.push(newBook)
+        console.log(myLibrary);
+    }
+
+}
+
+function createBook(bookInfo) {
+
+    for (let i = 0; i < bookInfo.length; i++) {
+        console.log(bookInfo[0].isTitle);
+        console.log(bookInfo[0].isAuthor);
+        console.log(bookInfo[0].isGenre);
+        console.log(bookInfo[0].isPages);
+        console.log(bookInfo[0].isRead);
+    }
+}
+
+Book.prototype.isRead = function () {
+    this.isRead = !this.isRead;
+    console.log(this.isRead);
+}
+
+let book = new Book('The Hobbit', 'J.K Rawlings', 'Sci-Fi', 98, true);
+console.log(book);
+console.log(book.isRead());
