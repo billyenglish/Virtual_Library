77ec8bccb9d90b014d960ea08a0fb7fd6ab34b4c
Demar English <demarenglish@Mac.attlocal.net>
2025-04-26 08:52:25 -0400

    Creating factory function.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
+function Book(title, author, genre, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.pages = pages;
+    this.read = read;
+}
+
 // Create DOM Logic
 
+/*
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu");
+const dialogModal = document.querySelector("dialog");
+
+
+hamburgerMenu.addEventListener("click", () => {
+    dialogModal.showModal();
+});
+
+closeMenu.addEventListener("click", () => {
+    dialogModal.close();
+});
+
+*/
