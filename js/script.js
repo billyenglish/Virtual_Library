b3d406300a2d74bc1995b54b71a1a6454ccc21fe
Demar English <demarenglish@Mac.attlocal.net>
2025-09-07 20:26:13 -0400

    creating user input.

--- js/script.js
+++ js/script.js
@@ -1,57 +1,118 @@
 class Book {
-    constructor(title, author, pages, genre, images) {
+    constructor(title, author, genre, pages, cover, read) {
         this.title = title;
         this.author = author;
-        this.pages = pages;
         this.genre = genre;
-        this.images = images;
-        this.read = false
+        this.pages = pages;
+        this.cover = cover;
+        this.read = read;
         this.id = crypto.randomUUID();
     }
 
+    toggleRead() {
+        return this.read != this.read;
+    }
 }
 
-class Library {
+class MyLibrary {
     constructor() {
         this.library = [];
     }
 
-    addBook(title, author, pages, genre, images) {
-        
-        if (title !== "" && author !== "" && pages > 0 && genre !== "") {
-            const newBook = new Book(title, author, pages, genre, images);
-            this.library.push(newBook);
+    getLibrary() {
+        return this.library;
+    }
+}
+
+class AddToLibrary {
+    constructor () {
+        this.currentLibrary = new MyLibrary;
+    }
+
+    getBookInfo(title, author, genre, pages, cover = '', read) {
+        const myLibrary = this.currentLibrary.getLibrary();
+
+        if (title && author && genre && pages > 0) {
+                const bookExist = myLibrary.some(currentBooks =>
+                currentBooks.title === title &&
+                currentBooks.author === author
+            );
+            if (!bookExist) {
+                const newBook = new Book(title, author, genre, pages, cover, read);
+                myLibrary.push(newBook);
+                return myLibrary;
+            } else {
+                return 'Title'
+            }
         } else {
-            console.log("Please fill out all fields correctly");
+            return "Please Fill Out All Required Fields!"
         }
     }
 
-    bookDuplicate() {
-        const currentLibrary = [];
-
-        for (const book of this.library) {
-            if (!currentLibrary.some(currentBook => currentBook.title === book.title && currentBook.author === book.author)) {
-                currentLibrary.push(book);
-            } else {
-                console.log(`Duplicate book found: ${book.title}`);
-            }
-        }
-
+    getLibrary() {
+        const currentLibrary = this.currentLibrary.getLibrary();
         return currentLibrary;
     }
+ }
 
-}
-
-const myLibrary = new Library();
-myLibrary.addBook("The Hobbit", "J.R.R. Tolkien", 295, "Fantasy", 'Image');
-myLibrary.addBook("The Hobbit", "J.R.R. Tolkien", 295, "Fantasy", 'Image');
-myLibrary.addBook("1984", "George Orwell", 328, "Dystopian", 'Image');
-myLibrary.addBook("To Kill a Mockingbird", "Harper Lee", 281, "Fiction", 'Image');
-myLibrary.addBook("", "Unknown Author", 150, "Mystery", 'Image');
-console.log(myLibrary.bookDuplicate());
-
-class UIModalDialog {
+class BookForm {
     constructor() {
-        
+        this.title = document.querySelector('#book_title').value;
+        this.author = document.querySelector('#book_author').value;
+        this.genre = document.querySelector('#book_genre').value;
+        this.pages = document.querySelector('#book_pages').value;
+        this.cover = document.querySelector('#book_cover').value;
+        this.progress = document.querySelector('#book_progress').value;
+    }
+
+    getBookFormValues() {
+        return {
+            title: this.title,
+            author: this.author,
+            genre: this.genre,
+            pages: this.pages,
+            cover: this.cover,
+            progress: this.progress,
+        }
     }
 }
+
+let book = new BookForm();
+console.log(book);
+
+class UIDialogModal {
+    constructor() {
+        this.addBookButton = document.querySelector('#add_book_btn');
+        this.closeFormButton = document.querySelector('#close_form_btn');
+        this.submitFormButton = document.querySelector('#book_form_submit');
+        this.dialogModal = document.querySelector('#dialog');
+    }
+
+    addEventListener() {
+
+        if (this.addBookButton) {
+            this.addBookButton.addEventListener('click', () => {
+                this.dialogModal.showModal();
+             });
+        }
+
+        if (this.closeFormButton) {
+            this.closeFormButton.addEventListener('click', () => {
+                this.dialogModal.close();
+            });
+        }
+
+        if (this.submitFormButton) {
+            this.submitFormButton.addEventListener('click', () => {
+                const bookForm = new BookForm();
+                const bookValues = bookForm.getBookFormValues();
+                const { title, author, genre, pages } = bookValues;
+                console.log(title, author, genre, pages);
+            })
+        }
+    }
+}
+
+
+let dialogModal = new UIDialogModal();
+dialogModal.addEventListener();
